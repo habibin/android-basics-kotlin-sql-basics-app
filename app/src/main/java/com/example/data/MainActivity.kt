fa6bf876dbd1ccd9773e5a205ef59146fc300460
habibin <habibin@oregonstate.edu>
2023-08-17 16:32:22 -0700

    created the textbook to type

--- app/src/main/java/com/example/data/MainActivity.kt
+++ app/src/main/java/com/example/data/MainActivity.kt
@@ -15,12 +15,14 @@
  */
 package com.example.data
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
